870f19ceb23d3ba02413a7602e887f28379aac39
Samsu Bahri <samsu.bahri@binus.ac.id>
2025-09-17 22:47:22 +0800

    fix produk list

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,12 +3,14 @@
     public partial class App : Application
     {
         public static string API_HOST { get; set; }
+        public static string IMAGE_HOST { get; set; }
         public App()
         {
             InitializeComponent();
 
             //MainPage = new AppShell();
             API_HOST = "http://192.168.77.8/_resto007/api/";
+            IMAGE_HOST = "http://192.168.77.8/_resto007/public/images/";
 
             MainPage =  new NavigationPage(new Transaksi.ProdukMenu());
         }
--- Transaksi/ProdukMenu.xaml.cs
+++ Transaksi/ProdukMenu.xaml.cs
@@ -1,18 +1,26 @@
-using System;
-using System.Text;
-using System.Net.Http;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using static Microsoft.Maui.Controls.Compatibility.Grid;
 
 namespace Resto2025.Transaksi;
 
 public partial class ProdukMenu : ContentPage
 {
     public string ID_KATEGORI = string.Empty;
+    public string ID_KONSUMEN = "1"; // Default ID_KONSUMEN GUEST
+
+    private List<list_produk> _listproduk;
     public ProdukMenu()
 	{
 		InitializeComponent();
         get_data_kategori();
+
+        _listproduk = new List<list_produk>();
+        get_listproduk();
     }
 
 
@@ -23,6 +31,65 @@ public partial class ProdukMenu : ContentPage
         public string id_kategori { get; set; } = string.Empty;
 
         
+
+    }
+
+    public class list_produk
+    {
+        public string id_produk { get; set; } = string.Empty; public string id_produk_sell { get; set; } = string.Empty;
+        public string kode_produk { get; set; } = string.Empty;
+        public string url_gambar => App.IMAGE_HOST + kode_produk + ".jpg";
+        public string nama_produk { get; set; } = string.Empty;
+        public int stok { get; set; } = 0;
+        public double harga_jual { get; set; } = 0; 
+        public string new_harga_jual { get; set; } = string.Empty;
+        public double opacity_produk { get; set; } = 1;
+        public bool enabled_produk { get; set; } = true;
+    }
+
+    private async Task get_listproduk()
+    {
+       // contentview.IsVisible = true;
+        await Task.Delay(1000);
+        string url = App.API_HOST + "produk/list_produk.php?id_kategori=" + ID_KATEGORI;
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            List<list_produk> rowData = JsonConvert.DeserializeObject<List<list_produk>>(json);
+            _listproduk.Clear();
+
+
+            for (int i = 0; i < rowData.Count; i++)
+            {
+
+                var formated = "Rp " + ((int)rowData[i].harga_jual).ToString("N0");
+                rowData[i].new_harga_jual = formated;
+                int cek_sisa = rowData[i].stok;
+                if (cek_sisa <= 0)
+                {
+                    rowData[i].opacity_produk = 0.3; rowData[i].enabled_produk = false;
+                }
+                _listproduk.Add(rowData[i]);
+            }
+
+            if (rowData.Count >= 1)
+            {
+                
+
+                CV_ListProduk.ItemsSource = _listproduk;
+            }
+            else if (rowData.Count <= 0)
+            {
+               
+            }
+
+            //T_TotalVariatif.Text = $"Total {kategori}: {rowData.Count.ToString()} Produk";
+        }
+
+
 
     }
 
@@ -40,6 +107,7 @@ public partial class ProdukMenu : ContentPage
             List<string> nama = JsonConvert.DeserializeObject<List<string>>(json);
 
             Picker_Kategori.ItemsSource = nama;
+            System.Diagnostics.Debug.WriteLine("Load Kategori Menu Berhasil");
         }
         else
         {
@@ -69,7 +137,7 @@ public partial class ProdukMenu : ContentPage
                 string idKategori = kategoriData[0].id_kategori;
 
                ID_KATEGORI = idKategori;
-
+               get_listproduk();
 
             }
             else
