ea1b64c6dbf4fc3743e46788002d11971217e956
samsu007 <itdev.nuansa@gmail.com>
2025-09-24 11:45:54 +0800

    fix

--- MetodePembayaran/Tunai_Modal.xaml.cs
+++ MetodePembayaran/Tunai_Modal.xaml.cs
@@ -5,7 +5,7 @@ namespace Resto2025.MetodePembayaran;
 public partial class Tunai_Modal : Popup
 {
     private double totalBelanja = 0;
-    //private string uangKonsumenString = "";
+  
     private double uangKonsumenValue = 0;
     public Tunai_Modal(double totalBelanja)
     {
@@ -19,7 +19,7 @@ public partial class Tunai_Modal : Popup
         Close();
     }
 
-    // Method untuk tombol angka (0-9) - Logika diubah menjadi numerik
+  
     private void Keypad_Clicked(object sender, EventArgs e)
     {
         var button = (Button)sender;
@@ -30,7 +30,6 @@ public partial class Tunai_Modal : Popup
         UpdateDisplay();
     }
 
-    // Method untuk tombol nominal cepat - Logika diubah menjadi PENJUMLAHAN
     private void Nominal_Clicked(object sender, EventArgs e)
     {
         var button = (Button)sender;
@@ -42,23 +41,19 @@ public partial class Tunai_Modal : Popup
         UpdateDisplay();
     }
 
-    // Method untuk tombol Clear (C) - Logika diubah menjadi reset angka
     private void Clear_Clicked(object sender, EventArgs e)
     {
         uangKonsumenValue = 0;
         UpdateDisplay();
     }
 
-    // Method utama untuk update tampilan - Sekarang lebih sederhana
     private void UpdateDisplay()
     {
-        // Update Label Uang Konsumen
+       
         L_UangKonsumen.Text = $"Rp {uangKonsumenValue:N0}";
 
-        // Hitung kembalian
         double kembalian = uangKonsumenValue - this.totalBelanja;
 
-        // Tampilkan kembalian jika uangnya cukup
         if (kembalian >= 0)
         {
             L_Kembalian.Text = $"Rp {kembalian:N0}";
