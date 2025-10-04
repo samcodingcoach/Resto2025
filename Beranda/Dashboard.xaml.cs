8f3c0996a1db9869eceee96f14dcc0dd61dd8633
Samsu Bahri <samsu.bahri@binus.ac.id>
2025-10-04 20:43:39 +0800

    fix info order

--- Beranda/Dashboard.xaml.cs
+++ Beranda/Dashboard.xaml.cs
@@ -6,14 +6,30 @@ namespace Resto2025.Beranda;
 public partial class Dashboard : ContentPage
 {
     string ID_USER = "4"; //Preferences.Get("ID_USER", string.Empty);
+    private List<list_order> _listorder;
     public Dashboard()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
+
         get_summary();
-	}
 
-   
+        _listorder = new List<list_order>(); // taruh di public load 
 
+        get_listorder();
+    }
+
+    public class list_order
+    {
+        public string id_order { get; set; } = string.Empty;
+        public string kode_payment { get; set; } = string.Empty;
+        public string waktu_terima { get; set; } = string.Empty;
+        public string id_tagihan { get; set; } = string.Empty;
+        public int total_item { get; set; } = 0;
+        public int siap { get; set; } = 0;
+        public int tersaji { get; set; } = 0;
+        public int batal { get; set; } = 0;
+
+    }
 
     public class list_summary
     {
@@ -27,8 +43,6 @@ public partial class Dashboard : ContentPage
 
     }
 
-
-   
     private async void get_summary()
     {
         try
@@ -85,4 +99,34 @@ public partial class Dashboard : ContentPage
 
 
 
+
+    private async void get_listorder()
+    {
+        string url = App.API_HOST + "dapur/order.php";
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            List<list_order> rowData = JsonConvert.DeserializeObject<List<list_order>>(json);
+
+            _listorder.Clear(); // Hapus list sebelum diisi
+
+            
+            for (int i = 0; i < rowData.Count; i++)
+            {
+
+                _listorder.Add(rowData[i]); 
+            }
+
+            //total = rowData.Count;
+            lv_dapurorder.ItemsSource = _listorder; 
+
+        }
+        else
+        {
+           
+        }
+    }
 }
