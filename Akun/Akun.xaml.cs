b8a895fb71c00a2eb0b93604a38eced4ff1bcd52
Samsu Bahri <samsu.bahri@binus.ac.id>
2025-10-06 21:21:33 +0800

    fix

--- Akun/Akun.xaml.cs
+++ Akun/Akun.xaml.cs
@@ -6,4 +6,24 @@ public partial class Akun : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void B_Update_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void B_UbahPassword_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void B_Logout_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapPromo_Tapped(object sender, EventArgs e)
+    {
+
+    }
 }
